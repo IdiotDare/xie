44990250a50d03dcafb151ad85506bc5d9fe53b1
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2021-11-27 20:33:25 -0500

    jhk

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 function myButton(newText){
+    let newText = "help"
     if(document.getElementById('maintext') != newText){
         document.getElementById('maintext') = newText;
-    } else{
+    } else {
         document.getElementById('maintext') = 'yeetos'
     }
 }
