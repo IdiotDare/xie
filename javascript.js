abbd7c8ff20601af0b311fd52784a477ccbac4ae
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2021-11-27 21:05:22 -0500

    help

--- javascript.js
+++ javascript.js
@@ -8,3 +8,30 @@ function myButton(){
         document.getElementById('maintext').innerHTML = x;
     }
 }
+//if document.cookie = 
+function getCookie(name) {
+    // Split cookie string and get all individual name=value pairs in an array
+    var cookieArr = document.cookie.split(";");
+    
+    // Loop through the array elements
+    for(var i = 0; i < cookieArr.length; i++) {
+        var cookiePair = cookieArr[i].split("=");
+        
+        /* Removing whitespace at the beginning of the cookie name
+        and compare it with the given string */
+        if(name == cookiePair[0].trim()) {
+            // Decode the cookie value and return
+            return decodeURIComponent(cookiePair[1]);
+        }
+    }
+    
+    // Return null if not found
+    return null;
+}
+
+let clickCookie = getCookie("clicked")
+if(clickCookie = "yes"){
+    document.getElementById('maintext').innerHTML = "help";
+} else {
+    alert('fucking failure you are')
+}
