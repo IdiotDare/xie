4eaa07005c795fef30cb3b41e16eaa6c26661add
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2021-11-27 20:46:36 -0500

    ssss

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 function myButton(){
     let newText = "help";
+    document.cookie = "clicked = yes;"
+    let x = document.cookie
     if(document.getElementById('maintext').innerHTML != newText){
         document.getElementById('maintext').innerHTML = newText;
     } else {
-        document.getElementById('maintext').innerHTML = 'yeetos'
+        document.getElementById('maintext').innerHTML = x;
     }
 }
